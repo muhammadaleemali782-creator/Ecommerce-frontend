0064845cf902c875e535570ff93983d6c84c4b8a
muhammadaleemali782-creator <muhammadaleemali782@gmail.com>
2026-07-09 15:31:54 +0530

    fix: remove misplaced bottom-nav spacer causing gap below header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -276,9 +276,6 @@ export default function Navbar({ setPage, cartCount, pageBadge = {}, noBottomMar
         ))}
       </nav>
 
-      {/* Mobile bottom spacer */}
-      <div style={{ display:isMobile?"block":"none", height:60 }} />
-
       {/* ✅ Logout Confirmation Modal */}
       {showLogoutConfirm && (
         <div style={{ position:"fixed", top:0, left:0, right:0, bottom:0, zIndex:300, display:"flex", alignItems:"center", justifyContent:"center" }}>
